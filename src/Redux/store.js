3965cd30322a2529034c024c83722985b47ee055
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-04-25 09:12:55 +0530

    remove the dev tool

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -13,8 +13,9 @@ const rootReducer = combineReducers({
 
 export const store = createStore(
     rootReducer,
-    compose(
-        applyMiddleware(ReduxThunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(ReduxThunk)
 )
+// compose(
+//     applyMiddleware(ReduxThunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
